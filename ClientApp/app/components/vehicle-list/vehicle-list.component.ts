5b930e93c2ed9e2823750965c14996006813c6b4
João Paulo Cardoso Façanha <jpcardoso89@outlook.com>
2017-08-01 10:50:58 -0300

    Correções na lista de vehicles
    Correções na lista de vehicles

--- ClientApp/app/components/vehicle-list/vehicle-list.component.ts
+++ ClientApp/app/components/vehicle-list/vehicle-list.component.ts
@@ -13,17 +13,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VehicleListComponent implements OnInit {
   vehicles: Vehicle[];
+  allVehicles: Vehicle[];
   makes: KeyValuePair[];
   filter:any = {};
   constructor(private vehicleService:VehicleService) { }
 
   ngOnInit() {
-    this.vehicleService.getVehicles().subscribe(vehicles => this.vehicles = vehicles);
+    this.vehicleService.getVehicles().subscribe(vehicles => this.vehicles = this.allVehicles = vehicles);
     this.vehicleService.getMakes().subscribe(makes => this.makes = makes);
   }
 
   onFilterChange(){
-    
+    let vehiclesFiltered = this.allVehicles;
+      
+    if(this.filter.makeId)
+      vehiclesFiltered = vehiclesFiltered.filter(v => v.make.id == this.filter.makeId);
+
+    if(this.filter.modelId){
+      vehiclesFiltered = vehiclesFiltered.filter(v => v.model.id == this.filter.modelId);
+
+    this.vehicles = vehiclesFiltered;
+    }
   }
 
 }
